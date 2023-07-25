36c2043024895a628f22edf827715cca036a128c
gusthelp102 <gusthelp102@gmail.com>
2023-07-25 21:03:41 +0300

    added fixture details

--- lib/screens/fixture_detail_screen.dart
+++ lib/screens/fixture_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../services/api_service.dart';
 
 class FixtureDetailScreen extends StatefulWidget {
@@ -14,6 +16,40 @@ class FixtureDetailScreen extends StatefulWidget {
 
 class _FixtureDetailScreenState extends State<FixtureDetailScreen> {
   String? selectedWinner;
+  Map<String, dynamic>? _apiData;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchFixtureData('592141');
+  }
+
+  Future<void> _fetchFixtureData(String id) async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://api-football-v1.p.rapidapi.com/v3/fixtures?id=$id'),
+        headers: {
+          'X-RapidAPI-Key':
+              '13c64b1909mshd0226621badc3dap10a2dcjsn2f557fb25460',
+          'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          _apiData = jsonDecode(response.body);
+          _apiData = _apiData![0];
+        });
+      } else {
+        // Handle error if API call fails
+        print('Error fetching fixture data: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching fixture data: $e');
+    }
+  }
+
+  final ApiService apiService = ApiService();
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -28,13 +64,13 @@ class _FixtureDetailScreenState extends State<FixtureDetailScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            FixtureCard(
-              homeTeamLogo: widget.fixtureData['teams']['home']['logo'],
-              homeTeamName: widget.fixtureData['teams']['home']['name'],
-              awayTeamLogo: widget.fixtureData['teams']['away']['logo'],
-              awayTeamName: widget.fixtureData['teams']['away']['name'],
-              advice: widget.fixtureData['predictions']['advice'],
-            ),
+            _apiData != null
+                ? FixtureCard(
+                    date: _apiData!['response']['fixture']['date'],
+                    venueName: _apiData!['response']['venue']['name'],
+                    venueCity: _apiData!['response']['venue']['city'],
+                  )
+                : CircularProgressIndicator(),
             SizedBox(height: 20),
             DropdownButton<String>(
               value: selectedWinner,
@@ -64,6 +100,11 @@ class _FixtureDetailScreenState extends State<FixtureDetailScreen> {
                 _submitPrediction();
               },
               child: Text('Submit Prediction'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightGreen, // Set button color to light green
+                onPrimary: Colors.white, // Set text color to white
+                fixedSize: Size(200, 50), // Set fixed size for the button
+              ),
             ),
           ],
         ),
@@ -88,6 +129,7 @@ class _FixtureDetailScreenState extends State<FixtureDetailScreen> {
       // Create a data map to store the prediction details
       final Map<String, dynamic> predictionData = {
         'userId': _auth.currentUser!.uid,
+        'fixtureId': widget.fixtureData['fixture']['id'],
         'leagueName': widget.fixtureData['league']['name'],
         'homeTeamName': widget.fixtureData['teams']['home']['name'],
         'awayTeamName': widget.fixtureData['teams']['away']['name'],
@@ -118,67 +160,63 @@ class _FixtureDetailScreenState extends State<FixtureDetailScreen> {
 }
 
 class FixtureCard extends StatelessWidget {
-  final String? homeTeamLogo;
-  final String? homeTeamName;
-  final String? awayTeamLogo;
-  final String? awayTeamName;
-  final String? advice;
+  final String? date;
+  final String? venueName;
+  final String? venueCity;
 
   FixtureCard({
-    this.homeTeamLogo,
-    this.homeTeamName,
-    this.awayTeamLogo,
-    this.awayTeamName,
-    this.advice,
+    this.date,
+    this.venueName,
+    this.venueCity,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 4,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (homeTeamLogo != null)
-                    Image.network(
-                      homeTeamLogo!,
-                      width: 48,
-                      height: 48,
-                    ),
-                  SizedBox(width: 10),
-                  Text(
-                    '$homeTeamName vs $awayTeamName',
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(width: 10),
-                  if (awayTeamLogo != null)
-                    Image.network(
-                      awayTeamLogo!,
-                      width: 48,
-                      height: 48,
-                    ),
-                ],
-              ),
-              SizedBox(height: 10),
-              if (advice != null)
-                Text(
-                  advice!,
-                  style: TextStyle(fontSize: 14),
-                  textAlign: TextAlign.center,
-                ),
-            ],
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
           ),
-        ),
+        ],
+      ),
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Fixture Date:',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            date ?? 'N/A',
+            style: TextStyle(fontSize: 14, color: Colors.black),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Venue:',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            '$venueName, $venueCity' ?? 'N/A',
+            style: TextStyle(fontSize: 14, color: Colors.black),
+          ),
+        ],
       ),
     );
   }
